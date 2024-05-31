d21f07a999be386c3579d2db9e124c3dc207370c
SortingFarmer <bengemrafdavid@gmail.com>
2024-05-31 20:00:08 +0200

    Document variablen ge-added und getter und setter

--- src/businessObjects/Document.java
+++ src/businessObjects/Document.java
@@ -1,4 +1,47 @@
 package businessObjects;
 
+import javax.xml.datatype.DatatypeConstants;
+
 public class Document {
+    private int id;
+    private String name;
+    private String path;
+    private String type;
+
+    public Document(String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
