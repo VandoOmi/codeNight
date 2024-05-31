550c99a5e441edb2d4b78b0930466c9d08cc7411
vando <vando.02800@gmail.com>
2024-05-31 21:32:30 +0200

    User

--- src/utils/UserUtil.java
+++ src/utils/UserUtil.java
@@ -1,11 +1,15 @@
 package utils;
 
+import businessObjects.User;
 import datenBanken.dao.BenutzerDao;
 
 public class UserUtil {
     public static boolean userAvailable(String name, String password){
         BenutzerDao dao = new BenutzerDao();
-        dao.
+        if(dao.read(name).getPassword().equals(password)) {
+            return true;
+        }
+        return false;
     }
 
 }
